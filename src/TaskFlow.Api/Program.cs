80f60c314ed9cd71b703f5cb6c72d1cab2e38023
DESKTOP-OF9VPN6\Admin <aminamughal27@gmail.com>
2026-07-03 00:36:05 +0300

    Azure Infrastructure & Key Vault Integration
    This includes:
    
    Azure App Service
    App Service Plan
    Azure SQL Database
    Managed Identity
    Azure Key Vault
    Key Vault integration with .NET
    JWT secret removed from appsettings.json

--- src/TaskFlow.Api/Program.cs
+++ src/TaskFlow.Api/Program.cs
@@ -1,8 +1,21 @@
 using FluentValidation;
 using TaskFlow.Api.Extensions;
+using Azure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var keyVaultUri = builder.Configuration["KeyVault:VaultUri"];
+
+if //(!builder.Environment.IsDevelopment() &&
+    (!string.IsNullOrWhiteSpace(keyVaultUri))
+{
+    builder.Configuration.AddAzureKeyVault(
+        new Uri(keyVaultUri),
+        new DefaultAzureCredential());
+    
+}
+Console.WriteLine("here is the JWT key");
+Console.WriteLine(builder.Configuration["Jwt:SecretKey"]);
 builder.ConfigureSerilog();
 
 builder.Services.AddApiServices(builder.Configuration);
